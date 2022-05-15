57beb46627f8c9db1d6ade26c6d2b65defe847c9
Luiz Oliveira <luizoliveira2808@gmail.com>
2022-05-15 19:47:34 -0300

    First commit

---
+++ server/src/routes.ts
@@ -0,0 +1,82 @@
+import express, { Request, Response } from "express";
+
+import { v4 as uuid } from "uuid";
+
+export const routes = express.Router();
+
+type Product = {
+  id: string;
+  name: string;
+  code: string;
+  created: string;
+}
+
+const products: Product[] = [];
+
+routes.post("/products", (request, response) => {
+  const { name, code } = request.body;
+
+  const product = {
+    id: uuid(),
+    name,
+    code,
+    created: new Date().toISOString(),
+  };
+
+  products.push(product);
+
+  return response.status(201).json(product);
+});
+
+routes.get("/products", (request, response) => {
+  return response.status(200).json(products);
+});
+
+routes.put("/products/:productId", (request: Request, response: Response) => {
+  const { productId } = request.params;
+
+  const { name, code } = request.body;
+
+  const product = products.find(product => product.id === productId);
+
+  if (!product) {
+    return response.status(404).json({
+      error: "Product not found"
+    });
+  }
+
+  product.name = name;
+  product.code = code;
+
+  return response.status(200).json(product);
+});
+
+routes.delete("/products/:productId", (request: Request, response: Response) => {
+  const { productId } = request.params;
+
+  const productIndex = products.findIndex(product => product.id === productId);
+
+  if (productIndex === -1) {
+    return response.status(404).json({
+      error: "Product not found"
+    });
+  }
+
+  products.splice(productIndex, 1);
+
+  return response.status(200).json({});
+});
+
+routes.get("/products/:productId", (request: Request, response: Response) => {
+  const { productId } = request.params;
+
+  const product = products.find(product => product.id === productId);
+
+  if (!product) {
+    return response.status(404).json({
+      error: "Product not found"
+    });
+  }
+
+  return response.status(200).json(product);
+});
